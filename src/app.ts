9472bbd918ad14c9284b9e8acfa6fbcf4d275d3e
TRoYHD <dark_angle.2010@hotmail.com>
2023-09-19 22:23:37 +0300

    fix issue with app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import bookRoutes from "./routes/bookRoutes"
 
 
 
 const app = express()
 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 app.use('/api/books', bookRoutes)
+
+
+
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000!");
